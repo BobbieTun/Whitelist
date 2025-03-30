5baea3ca824415af3b06a504e58dd8bb807ee90c
BobbieTun <130823077+BobbieTun@users.noreply.github.com>
2025-03-30 11:57:30 +0700

    Add files via upload

---
+++ WhiteList.lua
@@ -0,0 +1,10 @@
+return {
+    owners = {
+        ["MysteriousDaemon"] = true,
+        ["JakieSilli"] = true,
+        ["BobbieTun"] = true
+    },
+    buyers = {
+        ["ZaneOfSus_HGyA1"] = true
+    }
+}
