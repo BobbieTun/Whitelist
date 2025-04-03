e9f0cba9d4107ddd8a592f5653c95ecd67393fd0
BobbieTun <130823077+BobbieTun@users.noreply.github.com>
2025-04-03 09:36:25 +0700

    Update WhiteList.lua

--- WhiteList.lua
+++ WhiteList.lua
@@ -6,22 +6,7 @@ return {
     },
     buyers = {
         ["ZaneOfSus_HGyA1"] = true,
-        ["Hibran1986"] = true,
-        ["Hanz_Vladimir"] = true,
-        ["Hanz_VladimirAlt"] = true,
-        ["Bugoispro"] = true,
-        ["ELkhodi4531"] = true,
-        ["Izy_venom"] = true,
-        ["Izy_venom2"] = true,
-        ["Russelarigore22"] = true,
-        ["Markyy_0311"] = true,
-        ["The_LegendarkNinja"] = true,
         ["ducdayne1253"] = true,
-        ["YoungHoangDZ"] = true,
-        ["IamUnknownfacility"] = true,
-        ["BForPresident11"] = true, 
-        ["Khodi246810"] = true,
-        ["lenlen12345686"] = true,
         ["NkTaAnNn"] = true
     }
 }
